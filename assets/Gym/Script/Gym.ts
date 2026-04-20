aeebda295f1aefb3fcc8786eafb206e0779a8001
SummerViber96 <vdthu.it@gmail.com>
2026-04-20 07:40:21 +0700

    add fix

--- assets/Gym/Script/Gym.ts
+++ assets/Gym/Script/Gym.ts
@@ -7,12 +7,18 @@ export default class NewClass extends cc.Component {
     @property(cc.Node)
     npc: cc.Node = null
     @property(cc.Node)
-    listCusNode:cc.Node=null
-
+    listCusNode: cc.Node = null
+    arrCus = []
     start() {
         this.scheduleOnce(() => {
             cc.tween(this.npc).by(0.3, { opacity: -255, position: cc.v3(0, -80) }).start()
         }, 3)
+        for(let i=0;i<this.listCusNode.childrenCount;i++){
+            this.arrCus.push(this.listCusNode.children[i])
+        }
+    }
+    spawFistCustomer() {
+
     }
 
     // update (dt) {}
--- assets/Gym/Script/cusGym.ts
+++ assets/Gym/Script/cusGym.ts
@@ -6,13 +6,16 @@ export default class NewClass extends cc.Component {
 
     @property(cc.Node)
     pop: cc.Node = null
-
+    @property(sp.Skeleton)
+    anim:sp.Skeleton=null
     start() {
 
     }
-    showMision(){
+    showMision() {
         this.pop.getComponent(cc.Animation).play()
     }
+    move() {
 
+    }
     // update (dt) {}
 }
